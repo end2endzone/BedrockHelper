c9d550c0e99e8c86eba3551d8ca4b0f8e842ddce
Antoine Beauchamp <end2endzone@gmail.com>
2026-08-10 09:32:17 -0400

    Modified build script to force GOOS and GOARCH if unset. Updated output binary filename to `bedrock_helper-$GOOS-$GOARCH`.

--- ci/linux/build.sh
+++ ci/linux/build.sh
@@ -9,26 +9,45 @@ cd $PROJECTROOT/src
 VERSION=$(cat $PROJECTROOT/VERSION)
 COMMIT=$(git rev-parse --short HEAD)
 DATE=$(date +%F)
-CPU_ARCHITECTURE=$(uname -m)
-OS_TYPE=$(uname -s | tr '[:upper:]' '[:lower:]')
 
-# Truncate OS_TYPE when running on specific OS
-case "$OS_TYPE" in
-  linux)        OS_TYPE="linux" ;;
-  darwin)       OS_TYPE="macos" ;;
-  *mingw64*)    OS_TYPE="mingw64" ;;
-  *mingw32*)    OS_TYPE="mingw32" ;;
-  *msys*)       OS_TYPE="msys" ;;
-  *cygwin*)     OS_TYPE="cygwin" ;;
-  *)            OS_TYPE="unknown" ;;
-esac
+# GOOS
+if [[ -v GOOS ]]; then
+    echo "GOOS is set to: $GOOS"
+else
+    # Detect OS type
+    #OS_TYPE=$(uname -s | tr '[:upper:]' '[:lower:]')
+    ## Truncate OS_TYPE when running on specific OS
+    #case "$OS_TYPE" in
+    #linux)      GOOS="linux" ;;
+    #darwin)     GOOS="darwin" ;;
+    #*mingw64*|
+    #*mingw32*|
+    #*msys*|
+    #*cygwin*)   GOOS="windows" ;;
+    #*)          GOOS="unknown" ;;
+    #esac
+
+    GOOS=$(go env GOHOSTOS)
+    echo "GOOS is not set and is forced to: $GOOS"
+fi
+
+# GOARCH
+if [[ -v GOARCH ]]; then
+    echo "GOARCH is set to: $GOARCH"
+else
+    GOARCH=$(go env GOHOSTARCH)
+    echo "GOARCH is not set and is forced to: $GOARCH"
+fi
+
+# Ensures your binary does not depend on host operating system C libraries, making the binary completely portable.
+CGO_ENABLED=0
 
 # Point this to the exact package path where your main function lives
 PKG="main"
 
-echo "Building bedrock_helper-$OS_TYPE-$CPU_ARCHITECTURE version $VERSION..."
+echo "Building bedrock_helper-$GOOS-$GOARCH version $VERSION..."
 
 # Run build from the root, pointing to the main package directory
-go build -ldflags "-X '${PKG}.Version=${VERSION}' -X '${PKG}.CommitHash=${COMMIT}' -X '${PKG}.BuildDate=${DATE}'" -o $PROJECTROOT/bin/bedrock_helper-$OS_TYPE-$CPU_ARCHITECTURE ./cmd/bedrock_helper
+go build -ldflags "-X '${PKG}.Version=${VERSION}' -X '${PKG}.CommitHash=${COMMIT}' -X '${PKG}.BuildDate=${DATE}'" -o $PROJECTROOT/bin/bedrock_helper-$GOOS-$GOARCH ./cmd/bedrock_helper
 
 echo "Build complete!"
--- ci/windows/build.ps1
+++ ci/windows/build.ps1
@@ -8,20 +8,40 @@ Set-Location $ProjectRoot/src
 $Version = (Get-Content $ProjectRoot\VERSION).Trim()
 $Commit = (git rev-parse --short HEAD).Trim()
 $Date = (Get-Date).ToString("yyyy-MM-dd")
-$CpuArchitecture = switch ((Get-CimInstance Win32_Processor).Architecture) {
-    9       { "x64" }
-    12      { "arm64" }
-    0       { "386" }
-    default { "unknown" }
+
+# GOOS
+if (-not [string]::IsNullOrEmpty($env:GOOS)) {
+    Write-Output "GOOS is set to: $env:GOOS"
+} else {
+    $env:GOOS = (go env GOHOSTOS).Trim()
+    Write-Output "GOOS is not set and is forced to: $env:GOOS"
 }
-$OsType = "windows"
+
+# GOARCH
+if (-not [string]::IsNullOrEmpty($env:GOARCH)) {
+    Write-Output "GOARCH is set to: $env:GOARCH"
+} else {
+    # Detect OS type
+    #$env:GOARCH = switch ((Get-CimInstance Win32_Processor).Architecture) {
+    #    9       { "amd64" }
+    #    12      { "arm64" }
+    #    default { "unknown" }
+    #}
+
+    $env:GOARCH = (go env GOHOSTARCH).Trim()
+
+    Write-Output "GOARCH is not set and is forced to: $env:GOARCH"
+}
+
+# Ensures your binary does not depend on host operating system C libraries, making the binary completely portable.
+$env:CGO_ENABLED = "0"
 
 # Point this to the exact package path where your main function lives
 $Pkg = "main"
 
-Write-Host "Building bedrock_helper-$OsType-$CpuArchitecture.exe version $Version..."
+Write-Host "Building bedrock_helper-$env:GOOS-$env:GOARCH.exe version $Version..."
 
 # Run build from the root, pointing to the main package directory
-go build -ldflags "-X '$Pkg.Version=$Version' -X '$Pkg.CommitHash=$Commit' -X '$Pkg.BuildDate=$Date'" -o $ProjectRoot/bin/bedrock_helper-$OsType-$CpuArchitecture.exe ./cmd/bedrock_helper
+go build -ldflags "-X '$Pkg.Version=$Version' -X '$Pkg.CommitHash=$Commit' -X '$Pkg.BuildDate=$Date'" -o $ProjectRoot/bin/bedrock_helper-$env:GOOS-$env:GOARCH.exe ./cmd/bedrock_helper
 
 Write-Host "Build complete!"
