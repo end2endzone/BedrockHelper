f829919e885c7af2c55853314de7a9430232bd51
Antoine Beauchamp <end2endzone@gmail.com>
2026-08-10 08:41:22 -0400

    Renamed output executable from `bedrock_helper` to format `bedrock_helper-$OS_TYPE-$CPU_ARCHITECTURE`.

--- ci/linux/build.sh
+++ ci/linux/build.sh
@@ -9,13 +9,26 @@ cd $PROJECTROOT/src
 VERSION=$(cat $PROJECTROOT/VERSION)
 COMMIT=$(git rev-parse --short HEAD)
 DATE=$(date +%F)
+CPU_ARCHITECTURE=$(uname -m)
+OS_TYPE=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+# Truncate OS_TYPE when running on specific OS
+case "$OS_TYPE" in
+  linux)        OS_TYPE="linux" ;;
+  darwin)       OS_TYPE="macos" ;;
+  *mingw64*)    OS_TYPE="mingw64" ;;
+  *mingw32*)    OS_TYPE="mingw32" ;;
+  *msys*)       OS_TYPE="msys" ;;
+  *cygwin*)     OS_TYPE="cygwin" ;;
+  *)            OS_TYPE="unknown" ;;
+esac
 
 # Point this to the exact package path where your main function lives
 PKG="main"
 
-echo "Building BedrockHelper version $VERSION..."
+echo "Building bedrock_helper-$OS_TYPE-$CPU_ARCHITECTURE version $VERSION..."
 
 # Run build from the root, pointing to the main package directory
-go build -ldflags "-X '${PKG}.Version=${VERSION}' -X '${PKG}.CommitHash=${COMMIT}' -X '${PKG}.BuildDate=${DATE}'" -o $PROJECTROOT/bin/bedrock_helper ./cmd/bedrock_helper
+go build -ldflags "-X '${PKG}.Version=${VERSION}' -X '${PKG}.CommitHash=${COMMIT}' -X '${PKG}.BuildDate=${DATE}'" -o $PROJECTROOT/bin/bedrock_helper-$OS_TYPE-$CPU_ARCHITECTURE ./cmd/bedrock_helper
 
 echo "Build complete!"
--- ci/windows/build.ps1
+++ ci/windows/build.ps1
@@ -8,14 +8,20 @@ Set-Location $ProjectRoot/src
 $Version = (Get-Content $ProjectRoot\VERSION).Trim()
 $Commit = (git rev-parse --short HEAD).Trim()
 $Date = (Get-Date).ToString("yyyy-MM-dd")
+$CpuArchitecture = switch ((Get-CimInstance Win32_Processor).Architecture) {
+    9       { "x64" }
+    12      { "arm64" }
+    0       { "386" }
+    default { "unknown" }
+}
+$OsType = "windows"
 
 # Point this to the exact package path where your main function lives
 $Pkg = "main"
 
-Write-Host "Building BedrockHelper version $Version..."
+Write-Host "Building bedrock_helper-$OsType-$CpuArchitecture.exe version $Version..."
 
 # Run build from the root, pointing to the main package directory
-
-go build -ldflags "-X '$Pkg.Version=$Version' -X '$Pkg.CommitHash=$Commit' -X '$Pkg.BuildDate=$Date'" -o $ProjectRoot/bin/bedrock_helper.exe ./cmd/bedrock_helper
+go build -ldflags "-X '$Pkg.Version=$Version' -X '$Pkg.CommitHash=$Commit' -X '$Pkg.BuildDate=$Date'" -o $ProjectRoot/bin/bedrock_helper-$OsType-$CpuArchitecture.exe ./cmd/bedrock_helper
 
 Write-Host "Build complete!"
