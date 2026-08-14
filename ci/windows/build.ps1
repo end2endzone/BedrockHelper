9ff48b620a6eece326c107dd1e973d38b00f9aeb
Antoine Beauchamp <end2endzone@gmail.com>
2026-08-14 08:42:17 -0400

    Modified build scripts to change output file name to `bedrock_helper` without GOOS and GOARCH in the output filename. While building on a CI/CD server, keep the file name format to `bedrock_helper-$GOOS-$GOARCH` so that archiving and downloading is easier.

--- ci/windows/build.ps1
+++ ci/windows/build.ps1
@@ -3,6 +3,7 @@ $ErrorActionPreference = "Stop"
 # Get the projet's root directory
 $ProjectRoot = "$PSScriptRoot/../.."
 
+Push-Location
 Set-Location $ProjectRoot/src
 
 $Version = (Get-Content $ProjectRoot\VERSION).Trim()
@@ -21,16 +22,15 @@ if (-not [string]::IsNullOrEmpty($env:GOOS)) {
 if (-not [string]::IsNullOrEmpty($env:GOARCH)) {
     Write-Output "GOARCH is set to: $env:GOARCH"
 } else {
-    # Detect OS type
-    #$env:GOARCH = switch ((Get-CimInstance Win32_Processor).Architecture) {
-    #    9       { "amd64" }
-    #    12      { "arm64" }
-    #    default { "unknown" }
-    #}
-
     $env:GOARCH = (go env GOHOSTARCH).Trim()
-
     Write-Output "GOARCH is not set and is forced to: $env:GOARCH"
+}
+
+# Define the target binary file path based on the environment
+$Target="$ProjectRoot/bin/bedrock_helper.exe"
+if ($env:CI -eq "true") {
+    $Target="$ProjectRoot/bin/bedrock_helper-$env:GOOS-$env:GOARCH.exe"
+    echo "Building on CI/CD server. Changing the target file name to '$Target'"
 }
 
 # Ensures your binary does not depend on host operating system C libraries, making the binary completely portable.
@@ -39,9 +39,11 @@ $env:CGO_ENABLED = "0"
 # Point this to the exact package path where your main function lives
 $Pkg = "main"
 
-Write-Host "Building bedrock_helper-$env:GOOS-$env:GOARCH.exe version $Version..."
+Write-Host "Building $(Split-Path -Leaf $Target) version $Version..."
 
 # Run build from the root, pointing to the main package directory
-go build -ldflags "-X '$Pkg.Version=$Version' -X '$Pkg.CommitHash=$Commit' -X '$Pkg.BuildDate=$Date'" -o $ProjectRoot/bin/bedrock_helper-$env:GOOS-$env:GOARCH.exe ./cmd/bedrock_helper
+go build -ldflags "-X '$Pkg.Version=$Version' -X '$Pkg.CommitHash=$Commit' -X '$Pkg.BuildDate=$Date'" -o $Target ./cmd/bedrock_helper
 
 Write-Host "Build complete!"
+
+Pop-Location
